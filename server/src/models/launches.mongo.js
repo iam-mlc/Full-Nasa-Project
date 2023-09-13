232cb487ebf00179b22687c6d42bbb198b1147e7
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-13 09:39:50 +0200

    chore(launches.mongo): Add schema
    Implement schema for the launches model

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const types = {
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+};
+
+const launchesSchema = new mongoose.Schema(types);
