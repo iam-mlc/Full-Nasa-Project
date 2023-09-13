d4b0a9747ec99be76b213156514e3b2b71eb9dea
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-13 12:16:14 +0200

    refactor(planets.controller): Make controller async
    Make httpGetAllPlanets asynchronous

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,8 +1,12 @@
 const { getAllPlanets } = require("../../models/planets.model");
 
-function httpGetAllPlanets(req, res) {
+async function httpGetAllPlanets(req, res) {
+
+  const planets = await getAllPlanets();
+  
   //  The return statement is added to prevent multiple responses from being made. This statement only make one response and then ends the function. In this case it only retrns the planets as an array. NOTE: Check how the getAllPlanets() function works to understand why the data is an array
-  return res.status(200).json(getAllPlanets());
+
+  return res.status(200).json(planets);
 }
 
 module.exports = {
