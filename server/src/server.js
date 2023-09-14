a493f6ee19a2f71ff37f5c19951888713eef60e8
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-09-14 09:22:09 +0200

    fix(server): add await keyword to function
    - Add await keyword to connectDatabase function so that the rest of the code runs only when the database is ready

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ async function connectDatabase(){
 
 async function startServer() {
 
-  connectDatabase()
+  await connectDatabase()
 
   // The loadPlanetsData() function is added before the server.listen() because it returns a promise that has to be resolved before the server starts listening to requests. The promise is related to the habitable planets(or a data from a database) and since the planets data is part of a csv file, we need to parse(oe convert) it. Before parsing it we create a readable stream, which in turn is an asynchronous task. Every chunck (or piece) of data that is parsed and filtered, should be pushed into an array.The promise returned from this function is making sure that we only listen and respond to requests after the array is populated with data, or has set all the data in the array. If this was not done the habitable planets would always return an empty array (the assigned value of habitablePlanets, check the planets.model file). This function makes sure that the results are pushed to the array before listening to more requests
   await loadPlanetsData();
